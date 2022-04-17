601c88fe8a19a09de6e7fb81ba5d5b9d9ebfa208
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-04-17 13:17:53 +0300

    Added the constructor and promise

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -6,6 +6,7 @@ import { ProfileService } from 'src/app/profile.service';
 
 
 
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -16,10 +17,47 @@ export class UsersComponent implements OnInit {
   userName = 'gladys-gg';
   repos: Repos[] = [];
   users: User[] = [];
+  githubService: any;
+  errorMessage!: string;
+  loading!: boolean;
+  profileRequest: any;
 
   constructor(githubService:GithubService, profileService:ProfileService) { }
 
+
+  public getRepo(){
+    this.loading = true;
+    let promise = new Promise<void>((resolve, reject)=>{
+      this.githubService.getRepo( this.userName).toPromise().then((response: Repos[]) =>{
+        this.repos = response;
+        resolve();
+      },
+        (      error: any) =>{
+          this.errorMessage='An error was encountered';
+          this.loading = false;
+      }
+      
+      );
+    });
+    return promise;
+  }
+  public getUsers(){
+    this.loading = true;
+     let promise = new Promise<void>((resolve, reject)=>{
+       this.profileRequest.getUsers(this.userName).toPromise().then((response: any) =>{
+         this.users = response;
+         resolve();
+       },
+         (       error: any) =>{
+         this.errorMessage = "An error was encountered";
+         this.loading = false;
+       }
+       );
+     });
+     return promise;
+  }
   ngOnInit(): void {
   }
+  
 
 }
