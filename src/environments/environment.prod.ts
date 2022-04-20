283cc68ca0b712370a09d8e085547d02e931c925
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-04-20 11:15:19 +0300

    Auto-generated commit [ci skip]

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-// export const environment = {
-//   production: true
-// };
-
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.github.com/users/',
-  apiKey: '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'
-};
