e65628b5e3035619b16e31c224fd5d7d015b383f
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-04-20 11:05:10 +0300

    made changes to app routing,edited the navbar on the appcomponent and edited the environment file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubAppComponent } from './components/github-app/github-app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"Home",component:GithubAppComponent},  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
